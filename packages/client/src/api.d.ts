c5579f04e8397123c875cb0fdadc5743569e1dfc
ahmed rakan <ar.aldhafeeri11@gmail.com>
2025-09-16 13:56:35 +0300

    add node, npm version

---
+++ packages/client/src/api.d.ts
@@ -0,0 +1,6 @@
+export declare const API_BASE_URL: string;
+export declare const apiFetch: (endpoint: string, options?: RequestInit) => Promise<Response>;
+export declare const queryFetcher: ({ queryKey }: {
+    queryKey: any[];
+}) => Promise<any>;
+//# sourceMappingURL=api.d.ts.map
