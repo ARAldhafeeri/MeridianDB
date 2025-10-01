93f58ba2417bcf1bc64b0456a97a313ab3c23ad4
ahmed rakan <ar.aldhafeeri11@gmail.com>
2025-10-01 18:44:44 +0300

    behaviroal queue completed

---
+++ packages/behavioral-queue/src/BehavioralDurableObjectQueue.ts
@@ -0,0 +1,3 @@
+import { SimpleDurableObjectQueue } from "@meridiandb/shared/src/queue/SimpleDurableObjectQueue";
+
+export const BehavioralDurableObjectQueue = SimpleDurableObjectQueue;
--- packages/behavioral-queue/src/SimpleDurableObjectQueue.ts
+++
@@ -1,1 +0,0 @@
-// import { SimpleDurableObjectQueue } from "@/shared/";
