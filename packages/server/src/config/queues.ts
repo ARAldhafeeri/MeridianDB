8358890c06e3e592fb204cbae0db1dedee5362d6
ahmed rakan <ar.aldhafeeri11@gmail.com>
2025-10-02 18:23:21 +0300

    adding temporal, behavioral queues client, dead letter queue doesn't need client

---
+++ packages/server/src/config/queues.ts
@@ -0,0 +1,23 @@
+import { getSimpleQueueClient } from "@meridiandb/shared/src/queue/client/SimpleQueueClient";
+import { getQueuesEnvVariables } from "./context";
+
+/**
+ * Temporal Queue Client used in publishing temporal
+ * feature write on read to update access freqency
+ */
+export const temporalQueueClient = getSimpleQueueClient({
+  baseUrl: getQueuesEnvVariables().temporalQueueURL,
+  apiKey: getQueuesEnvVariables().temporalQueueApiKey,
+  maxPayloadSize: 1000,
+});
+
+/**
+ * Behavioral Queue Client used in publishing behavioral
+ * feature to update sucess, fail, and success rate of memories.
+ *
+ */
+export const behavioralQueueClient = getSimpleQueueClient({
+  baseUrl: getQueuesEnvVariables().behavioralQueueURL,
+  apiKey: getQueuesEnvVariables().behavioralQueueApiKey,
+  maxPayloadSize: 1000,
+});
